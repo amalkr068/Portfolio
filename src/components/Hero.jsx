810c13a0b4cb48b4309cff402eb2f514bb431f0a
amalkr068 <amalkr068@gmail.com>
2025-02-20 22:02:10 -0800

    alignments updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         I specialize in building modern and responsive web applications.
       </p>
       <div className='mt-8 space-x-4'>
-        <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact With Me</button>
+      <a href="#contact"><button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact With Me</button></a>
         <button className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Resume</button>
       </div>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className='bg-black text-white px-8 md:px-16 lg:px-24'>
+    <nav className='bg-black text-white px-8 md:px-16 lg:px-24 '>
       <div className='container py-2 flex justify-center md:justify-between items-center'>
             <div className='text-2xl font-bold hidden md:inline'>AMAL KR</div>
             <div className='space-x-6'>
@@ -12,7 +12,7 @@ const NavBar = () => {
                 <a href="#project" className='hover:text-gray-400'>Projects</a>
                 <a href="#contact" className='hover:text-gray-400'>Contact</a>
             </div>
-            <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Connect Me</button>
+           <a href="#contact"> <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 mt-1 rounded-full'>Connect Me</button></a>
       </div>
     </nav>
   )
